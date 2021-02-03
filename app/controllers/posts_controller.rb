be2b6167df1aa19f3b7e265e1de6961e581985d8
Oscar Silvera <ojsilvera@misena.edu.co>
2021-02-03 16:54:58 -0500

    implementar busqueda de posts publicados

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,8 +9,8 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.where(published: true)
-    if !params[:search].nil? && params[:search].nil?
-      @posts =PostSearchService.search(@posts, params[:search])
+    if !params[:search].nil? && params[:search].present?
+      @posts = PostsSearchService.search(@posts, params[:search])
     end
     render json: @posts, status: :ok
   end
---
+++ app/services/posts_search_service.rb
@@ -0,0 +1,6 @@
+class PostsSearchService
+  def self.search(curr_posts, query)
+    curr_posts.where("title like '%#{query}%'")
+  end
+
+end
