566e793e3483abff6569c1148b01f43cf52ccd6a
Oscar Silvera <ojsilvera@misena.edu.co>
2021-02-04 15:06:53 -0500

    implementar servicio de genearcio de token seguro

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
   validates :auth_token, presence: true
+
+  after_initialize :generate_auth_token
+
+  def generate_auth_token
+    unless auth_token.present?
+      self.auth_token = TokenGenerationService.generate
+    end
+  end
 end
